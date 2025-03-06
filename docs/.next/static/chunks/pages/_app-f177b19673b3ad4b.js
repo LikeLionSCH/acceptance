d559f7fa097455c52a2c343c5ec892f7a8b03ab8
iubns <iubns@naver.com>
2025-03-06 22:49:22 +0900

    fix: _next path

---
+++ docs/.next/static/chunks/pages/_app-f177b19673b3ad4b.js
@@ -0,0 +1,17 @@
+;(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+  [636],
+  {
+    4454: (_, n, p) => {
+      ;(window._next_P = window._next_P || []).push([
+        "/_app",
+        function () {
+          return p(8153)
+        },
+      ])
+    },
+  },
+  (_) => {
+    var n = (n) => _((_.s = n))
+    _.O(0, [593, 792], () => (n(4454), n(2923))), (_N_E = _.O())
+  },
+])
---
+++ docs/.next/static/chunks/pages/_error-15fcf437dbbcade0.js
@@ -0,0 +1,17 @@
+;(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+  [731],
+  {
+    9564: (_, n, e) => {
+      ;(window._next_P = window._next_P || []).push([
+        "/_error",
+        function () {
+          return e(2696)
+        },
+      ])
+    },
+  },
+  (_) => {
+    var n = (n) => _((_.s = n))
+    _.O(0, [636, 593, 792], () => n(9564)), (_N_E = _.O())
+  },
+])
